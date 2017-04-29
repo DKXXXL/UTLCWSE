a768e3af65f11ad955e8df00ebd7e670fbcfbc4d
EDJ <jinende1997@live.com>
2017-04-29 10:26:42 +0800

    Little Change.

--- ARVM.v
+++ ARVM.v
@@ -22,6 +22,8 @@ Module ARVM.
   Inductive R_Syscall : Type :=
   | _rs_obj : nat -> R_Syscall.
 
+
+  Require Import JMeq.
   
   Inductive R_op : Type :=
   | _ro_add : forall id1 id2 id3 (x: R_Ty 1),
@@ -29,27 +31,38 @@ Module ARVM.
                 R_pt id2 x ->
                 R_pt id3 x ->
                 R_op
-  | _ro_cons : forall id1 id2 id3 x' y' (x:R_Ty x') (y: R_Ty y') ,
+  (*
+   _ro_car; _ro_cdr : 
+   _ro_set_car; _ro_set_cdr
+   *)
+                  
+  | _ro_alloc : forall id1 x (y: R_Ty x),
+                  JMeq y _rt_cons ->
+                  R_pt id1 y ->
+                  R_op
+  | _ro_cons : forall id1 id2 id3 x1 x2 (y1 : R_Ty x1) (y2 : R_Ty x2),
                  R_pt id1 _rt_cons ->
-                 R_pt id2 x ->
-                 R_pt id3 y ->
+                 R_pt id2 y1 ->
+                 R_pt id3 y2 ->
                  R_op
-  | _ro_strloc : forall id1 x ,
-                   R_pt id1 (_rt_str x) ->
-                   R_op
-  | _ro_symloc : forall id1 x ,
-                   R_pt id1 (_rt_qsym x)->
-                   R_op
+  | _ro_car : forall id1 id2 x (y : R_Ty x),
+                R_pt id1 y ->
+                R_pt id2 _rt_cons ->
+                R_op
+  | _ro_cdr : forall id1 id2 x (y: R_Ty x),
+                R_pt id1 y ->
+                R_pt id2 _rt_cons ->
+                R_op
   | _ro_cast : forall id1 id2 x' (x : R_Ty x') (y :R_Ty x'),
                  R_pt id1 x ->
                  R_pt id2 y ->
                  R_op
-  | _ro_input : forall id1 x (y: R_Ty x),
-                  R_pt id1 y ->
+  | _ro_input : forall id1 x,
+                  R_pt id1 (_rt_str x) ->
                   R_Syscall ->
                   R_op
-  | _ro_output : forall id1 x (y:R_Ty x),
-                   R_pt id1 y ->
+  | _ro_output : forall id1 x,
+                   R_pt id1 (_rt_str x) ->
                    R_Syscall ->
                    R_op
   | _ro_label : nat -> R_op
@@ -59,14 +72,18 @@ Module ARVM.
               list R_op ->
               list R_op ->
               R_op
-  | _ro_assgn1 : forall id1 id2 x (y:R_Ty x),
+  | _ro_assgn : forall id1 id2 x (y:R_Ty x) (y' : R_Ty x),
+                  R_pt id1 y ->
+                  R_pt id2 y' ->
+                  R_op
+  (* repoint the pointer to new pointer*)
+  | _ro_mutate : forall id1 id2 (size start continue : unit_ptlong)
+                        (y: R_Ty size) (z: R_Ty continue) ,
                    R_pt id1 y ->
-                   R_pt id2 y ->
+                   size > start + continue ->
+                   R_pt id2 z ->
                    R_op
-  | _ro_assgn2 : forall id1 x (y:R_Ty x),
-                   R_pt id1 y ->
-                   R_Const y ->
-                   R_op
+  (* Mutate the object pointed by the pointer*)
   | _ro_syscall : R_Syscall -> R_op.
 
 End ARVM.
