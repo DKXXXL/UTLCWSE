3025c9fe0b851186ebe30bf043531d142dc14e9c
EDJ <jinende1997@gmail.com>
2017-08-24 02:09:37 -0400

    Working on ARVM. Now it can translated to C.

--- ARVM.v
+++ ARVM.v
@@ -1,8 +1,20 @@
 (* Non-Pointer (All Referenced) Virtual Machine *)
 
+Require Import SfLib.
+Require Import Coq.Strings.String.
+
+
 Module ARVM.
 
   Definition unit_ptlong := nat.
+  Definition literal := string.
+  Definition syscall_id := nat.
+  Definition typename := string.
+
+  (* Weak typing *)
+  (* Fixed Sized typing *)
+  (* No customized typing *)
+  
   
   Inductive R_Ty : unit_ptlong -> Type :=
   | _rt_int : R_Ty 1
@@ -10,20 +22,39 @@ Module ARVM.
   | _rt_dbl : R_Ty 1
   | _rt_bool : R_Ty 1
   | _rt_char : R_Ty 1
-  | _rt_cons : R_Ty 2
+  | _rt_pointer : forall size, R_Ty size -> R_Ty 1
+  (* A meta-type. For expressability. *)
   (* fixed length*)                  
-  | _rt_str : forall n, R_Ty n
-  | _rt_qsym : forall n, R_Ty n.
+  | _rt_str : forall n, R_Ty n (* Str *)
+  | _rt_qsym : forall n, R_Ty n (* Quote, symbol, meta*)
+  | _rt_data : forall n, typename -> R_Ty n 
+  (* Customized Data type, 
+  typename is annotation
+  customerized type is identified by name and size
+  *)
+  .
   
-  Inductive R_pt {x:unit_ptlong} : nat -> R_Ty x -> Type :=.
+  
+  Inductive R_Const {x:unit_ptlong} : literal -> R_Ty x -> Type :=
+  | _r_const : forall const ty, R_Const const ty. 
 
-  Inductive R_Const {x:unit_ptlong} : R_Ty x -> Type :=.
+  Inductive R_pt {x:unit_ptlong} : id -> R_Ty x -> Type :=
+  | _r_alloc : forall id ty, R_pt id ty
+  | _r_cast : forall id (y z : R_Ty x), 
+                R_pt id y ->
+                R_pt id z
+  | _r_ro : forall id ty const,
+              R_Const const ty ->
+              R_pt id ty.
+  (* Read only data retrieved. 
+  Get the pointer points to the read_only data *)
+
+
+
 
   Inductive R_Syscall : Type :=
-  | _rs_obj : nat -> R_Syscall.
+  | _rs_obj : syscall_id -> R_Syscall.
 
-
-  Require Import JMeq.
   
   Inductive R_op : Type :=
   | _ro_add : forall id1 id2 id3 (x: R_Ty 1),
@@ -34,7 +65,7 @@ Module ARVM.
   (*
    _ro_car; _ro_cdr : 
    _ro_set_car; _ro_set_cdr
-   *)
+   
                   
   | _ro_alloc : forall id1 x (y: R_Ty x),
                   JMeq y _rt_cons ->
@@ -57,6 +88,7 @@ Module ARVM.
                  R_pt id1 x ->
                  R_pt id2 y ->
                  R_op
+                 *)
   | _ro_input : forall id1 x,
                   R_pt id1 (_rt_str x) ->
                   R_Syscall ->
@@ -72,18 +104,32 @@ Module ARVM.
               list R_op ->
               list R_op ->
               R_op
-  | _ro_assgn : forall id1 id2 x (y:R_Ty x) (y' : R_Ty x),
+  (*
+  | _ro_repoint : forall id1 id2 x (y:R_Ty x) (y' : R_Ty x),
                   R_pt id1 y ->
                   R_pt id2 y' ->
-                  R_op
+                  R_op *)
   (* repoint the pointer to new pointer*)
-  | _ro_mutate : forall id1 id2 (size start continue : unit_ptlong)
+  (** Deprecated: I don't need some pointer to be mutable **)
+  | _ro_assign : forall src dst x (y:R_Ty x) ,
+                  R_pt src y ->
+                  R_pt dst y ->
+                  R_op
+  (* Copy the object from src to dst, weaker than mutate, type-safe *)
+
+  | _ro_set' : forall src dst x (y:R_Ty x) (y' : R_Ty x),
+                  R_pt src y ->
+                  R_pt dst y' ->
+                  R_op
+  (* Copy the object from src to dst, weaker than mutate, type-unsafe *)
+
+  | _ro_mutate : forall src dst (size start continue : unit_ptlong)
                         (y: R_Ty size) (z: R_Ty continue) ,
-                   R_pt id1 y ->
+                   R_pt src y ->
                    size > start + continue ->
-                   R_pt id2 z ->
+                   R_pt dst z ->
                    R_op
-  (* Mutate the object pointed by the pointer*)
+  (* Copy the data from src to dst*)
   | _ro_syscall : R_Syscall -> R_op.
 
 End ARVM.
